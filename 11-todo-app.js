1693070acc466b3761d1dcc959342f094e078b3b
Alex <alexnjoroge102@gmail.com>
2025-01-22 19:30:41 +0300

    added new functionality with array and loops, accumulator pattern

--- 11-todo-app.js
+++ 11-todo-app.js
@@ -1,16 +1,36 @@
 // version one of the app. displaying the todo list as an array on the console
-const todoList =[];
+const todoList =['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+function renderTodoList() {
+
+      let todoListHTML = ``;
+
+      for (let i = 0; i < todoList.length; i++ ) {
+        const todo = todoList[i] ;
+        const html = `<p>${todo}</p>`
+        todoListHTML += html;
+      }
+
+      console.log(todoListHTML);
+
+      document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
+}
 
 function addTodo(){
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
-  
-  
-  todoList.push(name);
-  console.log(todoList);
+      const inputElement = document.querySelector('.js-name-input');
+      const name = inputElement.value;
+      
+      
+      todoList.push(name);
+      console.log(todoList);
 
-  inputElement.value = '';
-}
+      inputElement.value = '';
+
+      renderTodoList();       
+    }
 
 //version 2 of the app
 
