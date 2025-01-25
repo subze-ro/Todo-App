6d884c2dd6dc2288e7fec69304358028e61db139
Alex <alexnjoroge102@gmail.com>
2025-01-25 18:43:42 +0300

    improved the game with addEventListeners instead of onclick. Also seen a use of addEventListenerAll which was cool

--- 12-todo-app-advanced-functions.js
+++ 12-todo-app-advanced-functions.js
@@ -20,11 +20,7 @@ function renderTodoList() {
         `<div>${name}</div>
          <div>${dueDate}</div>
          
-        <button onclick"
-            todoList.splice(${index}, 1);
-            renderTodoList();
-
-        " class="delete-todo-button">Delete</button>`;
+        <button class="delete-todo-button js-delete-todo-button">Delete</button>`;
 
         todoListHTML += html;
         
@@ -32,7 +28,21 @@ function renderTodoList() {
       
       document.querySelector('.js-todo-list')
       .innerHTML = todoListHTML;
-}
+
+      document.querySelectorAll('.js-delete-todo-button')
+      .forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+          todoList.splice(index, 1);
+          renderTodoList();
+        })
+      });
+          
+    }
+
+    document.querySelector('.js-add-todo-button')
+    .addEventListener('click', () => {
+      addTodo()
+    })
 
 function addTodo(){
       const inputElement = document.querySelector('.js-name-input');
